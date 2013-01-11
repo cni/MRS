f71db4aeefd7f507b6a32ba6b745219f45348683
arokem <arokem@gmail.com>
2013-01-11 12:26:16 -0800

    Add an ipython notebook for the analysis. Still working things out.

--- analysis.py
+++ analysis.py
@@ -7,25 +7,33 @@ import files as io
 import nitime as nt
 import nitime.analysis as nta
 
-data_files = ['mrs_P71680.7', 'mrs_P76288.7', 'pure_gaba_P64024.7']
+data_files = ['mrs_P71680.7']#, 'mrs_P76288.7', 'pure_gaba_P64024.7']
 data_dir = '/Users/arokem/projects/MRS/data/' 
 sampling_rate = 128
 
 fig, ax = plt.subplots(1)
 
+
 for file_name in data_files:
     data = io.get_data(os.path.join(data_dir, file_name))
     data = data.squeeze()
-    data = np.mean(data, -1)
+    # Average over repeats.
     data = np.mean(data, 1)
-    ts = nt.TimeSeries(data=np.abs(data).T, sampling_rate=sampling_rate)
-    S = nta.SpectralAnalyzer(ts)
-    f,c = S.spectrum_fourier
-    ax.plot(f[:500], np.abs((c[0] - c[1]))[:500], label=file_name)
+    ft = np.fft.fft(data)
+    
+    #data = np.mean(data[0])
+    #ts = nt.TimeSeries(data=data.T, sampling_rate=sampling_rate)
+
+    #S = nta.SpectralAnalyzer(ts)
+    #f,c = S.spectrum_fourier
+    # ax.plot(f[:500], np.abs((c[0] - c[1]))[:500], label=file_name)
     #ax.plot(f, np.abs((c[0] - c[1])))
-    ax.set_xlabel('Frequncy (MHz)')
+    #ax.plot(f, np.abs(np.fft.fftshift(c[0])))
+    #ax.plot(np.abs(ft))
+    #ax.set_xlabel('Frequency (MHz)')
     ax.set_ylabel('Power (TE1 - TE2)')
 
 fig.set_size_inches([10,10])
 plt.legend()
 plt.show()
+
