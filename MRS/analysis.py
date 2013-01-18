3befd9353698ff988f35772f53b52ef0bfaf72fe
arokem <arokem@gmail.com>
2013-01-17 17:14:03 -0800

    Moving stuff into the analysis module (get_spectra). Also, separate on/off resonance signals (but which is which?).

--- MRS/analysis.py
+++ MRS/analysis.py
@@ -5,9 +5,12 @@ import matplotlib.pyplot as plt
 
 import files as io
 import nitime as nt
+import nitime.timeseries as nts
 import nitime.analysis as nta
 
-def coil_combine(data, w_idx=[1,2,3], sampling_rate=5000.0, NFFT=640):
+import MRS.utils as ut
+
+def coil_combine(data, on_off=0, w_idx=[1,2,3], sampling_rate=5000.0, NFFT=640):
     """
     Combine data across coils based on the amplitude of the water peak,
     according to:
@@ -58,7 +61,7 @@ def coil_combine(data, w_idx=[1,2,3], sampling_rate=5000.0, NFFT=640):
     mean_psd = np.mean(psd_w.squeeze(), -1)
     mean_psd = np.mean(mean_psd, 2)
     # Average across off-/on-resonance (they're very similar anyway):
-    mean_psd = np.mean(mean_psd, -1) 
+    mean_psd = mean_psd[..., on_off]
     w = np.power(mean_psd/np.sqrt(np.sum(mean_psd**2)),2)
     w = mean_psd/np.sqrt(np.sum(mean_psd))
     # Shorthand:
@@ -70,8 +73,49 @@ def coil_combine(data, w_idx=[1,2,3], sampling_rate=5000.0, NFFT=640):
     weighted_w_supp_data = np.sum(w * w_supp_data.squeeze().T, 0)
     return weighted_w_data, weighted_w_supp_data
 
+def get_spectra(data, sampling_rate=5000.0,
+                filt_method=dict(lb=1.8, ub=600., filt_order=128),
+                spect_method=dict(NFFT=128, BW=6)):
+    """
+    Derive the spectra from MRS data
 
+    Parameters
+    ----------
+    data : tuple
+       (w_data, w_supp_data), where w_data contains data that is not
+       water-suppressed (contains a huge water peak in the spectrum) and
+       w_supp_data is water-suppressed. This is the output of `coil_combine`s 
 
+    
+    """
+    w_data, w_supp_data = data
+
+    ts_w = ut.apodize(nts.TimeSeries(np.mean(w_data, 1),
+                                     sampling_rate=sampling_rate))
+    
+    ts_nonw = ut.apodize(nts.TimeSeries(np.mean(w_supp_data, 1),
+                                        sampling_rate=sampling_rate))
+    f_ts_w = nta.FilterAnalyzer(ts_w, **filt_method).fir
+    f_ts_nonw = nta.FilterAnalyzer(ts_w, **filt_method).fir
+    S_w = nta.SpectralAnalyzer(ts_w,
+                               method=dict(NFFT=spect_method['NFFT']),
+                               BW=spect_method['BW'])
+    S_nonw = nta.SpectralAnalyzer(ts_nonw,
+                                  method=dict(NFFT=spect_method['NFFT']),
+                                  BW=spect_method['BW'])
+
+    
+    f_w, c_w = S_w.spectrum_multi_taper
+    f_nonw, c_nonw = S_nonw.spectrum_multi_taper
+
+    # Extract only the real part of the spectrum:
+    w_sig = np.real(c_w)
+    nonw_sig = np.real(c_nonw)
+
+    # Return the tuple (f_w should be the same as f_nonw, so return only one of
+    # them):
+    return f_w, w_sig, nonw_sig
+    
 if "__name__" == "__main__":
     # There will be some testing in here
     pass
