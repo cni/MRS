7b977d48bd85b8a15c32c8463f441284a6c6cc1f
Grace <tsmgrace@gmail.com>
2013-01-22 11:30:46 -0800

    modified bootstrap

--- gaba_bootstrap.py
+++ gaba_bootstrap.py
@@ -7,7 +7,7 @@ import scipy
 import scikits.bootstrap as bootstrap  
 
 from MRS.quant import *
-from MRS.analysis import get_spectra
+from MRS.analysis import get_spectra, get_single_spectra
 import MRS.files as io
 
 pfile = str(sys.argv[1])
@@ -20,14 +20,13 @@ data = data.squeeze() # get rid of singleton dimensions
 #np.save(os.path.join(data_dir,pfile[:-2]), data) # save in .npy format
 
 #amp_on, amp_off = reconstruct(pfile)
-quantify(pfile)
+#quantify(pfile)
 
 hdr = io.get_header(os.path.join(data_dir, pfile))
 nTransients = (hdr['nframes']-4)*2
 print 'number of transients: ' + str(nTransients) # which one is number of transients?
 
 ### bootstrap ###
-# separate water and water-suppressed signal
-# 
+w_data, w_supp_data = coil_combine(data)
 
-CIs = bootstrap.ci(data, statfunction=get_spectra, alpha=0.05, n_samples=10000, method='bca')
+CIs = bootstrap.ci(w_supp_data, statfunction=get_single_spectra, alpha=0.05, n_samples=10000, method='bca')
