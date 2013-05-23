8f35a6646fc085955a07d376230033a8dd1af1e8
arokem <arokem@gmail.com>
2013-05-23 16:56:38 -0700

    Update analysis script with water correction and new conventions of data shape.
    Also : made more optional inputs for the spectral analysis and filtering now
    optional (since water subtraction seems to take care of the low-freq stuff).

--- bin/mrs-analyze.py
+++ bin/mrs-analyze.py
@@ -18,8 +18,8 @@ parser = arg.ArgumentParser('Calculate MRS spectra from P files')
 parser.add_argument('in_file', action='store', metavar='File', 
                     help='MRS P file')
 
-parser.add_argument('out_file', action='store', metavar='File', 
-                    help='Output file (.csv)')
+parser.add_argument('--out_file', action='store', metavar='File', 
+                    help='Output file (.csv)', default=False)
  
 parser.add_argument('--sampling_rate', action='store', metavar='Float',
                     help='Sampling rate (default : 5000.0 Hz)',
@@ -33,6 +33,13 @@ parser.add_argument('--max_ppm', action='store', metavar='Float',
                     help='The minimal frequency (default: 4.3 ppm)',
                     default=4.3)
 
+parser.add_argument('--broadening', action='store', metavar='Float',
+                    help='Line broadening (Hz; default: 2)', default=2.0)
+
+parser.add_argument('--zero_fill', action='store', metavar='Float',
+                    help='Zero filling (number of points; default=100',
+                    default=100)
+
 parser.add_argument('--plot', action='store', metavar='Bool',
                     help='Whethere to produce a plot',
                     default=False)
@@ -43,10 +50,16 @@ if __name__ == "__main__":
     # Get data from file: 
     data = fi.get_data(in_args.in_file)
     # Use the water unsuppressed data to combine over coils:
-    w_data, w_supp_data = ana.coil_combine(data.squeeze())
+    w_data, w_supp_data = ana.coil_combine(data)
+    # Remove residual effects of water
+    w_supp_data = ana.subtract_water(w_data, w_supp_data)
     # Once we've done that, we only care about the water-suppressed data
-    f_nonw, nonw_sig = ana.get_spectra(nt.TimeSeries(w_supp_data,
-                                        sampling_rate=in_args.sampling_rate))
+
+    # Do spectral analysis:
+    f_nonw, nonw_sig = ana.get_spectra(w_supp_data,
+                                       line_broadening=float(in_args.broadening),
+                                       zerofill=in_args.zero_fill,
+                                       filt_method=None)
 
     f_ppm = ut.freq_to_ppm(f_nonw)
     idx0 = np.argmin(np.abs(f_ppm - in_args.min_ppm))
@@ -55,11 +68,13 @@ if __name__ == "__main__":
     # Convert from Hz to ppm and extract the part you are interested in.
     f_ppm = f_ppm[idx]
 
-    # The first echo (off-resonance) is in the first output 
-    echo1 = nonw_sig[0][:,idx]
-    # The on-resonance is in the second:
-    echo2 = nonw_sig[1][:,idx]
+    print nonw_sig.shape
     
+    # The first echo (off-resonance) is in the first output 
+    echo1 = nonw_sig[:,0][:,idx]
+    # The on-resonance is in the second:
+    echo2 = nonw_sig[:,1][:,idx]
+
     # Pack it into a recarray:
     names = ('ppm', 'echo1', 'echo2', 'diff')
     formats = (float, float, float, float)
@@ -67,11 +82,14 @@ if __name__ == "__main__":
     m_e1 = np.mean(echo1, 0)
     m_e2 = np.mean(echo2, 0)
     diff = m_e2 - m_e1
-    prep_arr = [(f_ppm[i], m_e1[i], m_e2[i], diff[i]) for i in range(len(f_ppm))]
-    out_array = np.array(prep_arr, dtype=dt)
 
-    # And save to output:
-    mlab.rec2csv(out_array, in_args.out_file)
+    if in_args.out_file:
+        prep_arr = [(f_ppm[i], m_e1[i], m_e2[i], diff[i])
+                              for i in range(len(f_ppm))]
+        out_array = np.array(prep_arr, dtype=dt)
+
+        # And save to output:
+        mlab.rec2csv(out_array, in_args.out_file)
 
     if in_args.plot:
         fig, ax = plt.subplots(3)
