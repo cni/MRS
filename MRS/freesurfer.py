143c9351f78f1ab7564c5cbed23339da7a396003
mirri66 <tsmgrace@gmail.com>
2013-08-06 14:50:21 -0700

    added subjID option and changed subjdir optional parameter

--- MRS/freesurfer.py
+++ MRS/freesurfer.py
@@ -4,13 +4,14 @@ freesurfer tools
 
 import os
 import argparse
+import re
 import nipype.pipeline.engine as pe
 import nipype.interfaces.io as nio
 import nipype.interfaces.freesurfer as fs
 import nipype.interfaces.utility as util
 import nipype.interfaces.fsl as fsl   
 
-def reconall(subjfile,*args): 
+def reconall(subjfile,subjID=None,subjdir=None): 
     """
     Carries out Freesurfer's reconall on T1 nifti file
     http://nipy.sourceforge.net/nipype/users/examples/smri_freesurfer.html
@@ -20,7 +21,10 @@ def reconall(subjfile,*args):
     subjfile: nifti file
         Path to subject's T1 nifti file
     
-    args: the directory to where segmentation results should be saved can be enetered as an optional second input parameter. Defaults to same directory as subjfile.  
+    subjID: string
+        optional name for subject's output folder
+
+    subjdir: the directory to where segmentation results should be saved. Defaults to same directory as subjfile.  
     """  
     parser = argparse.ArgumentParser()
 	
@@ -28,12 +32,16 @@ def reconall(subjfile,*args):
     filename = os.path.basename(subjfile)
 
     # Tell freesurfer what subjects directory to use
-    if len(args)>0:
-        subjdir=args[0]
-    else:
+    if subjdir==None:
         subjdir=T1dir
     fs.FSCommand.set_default_subjects_dir(subjdir)
     print 'saving to ' + subjdir
+
+    # subject ID
+    if subjID==None:
+        m=re.search('(\w+?)_*_',subjfile)
+        subjID=m.group(0) + 'seg'        
+        
 
     # check if file exists
     if os.path.isfile(subjfile):
@@ -45,13 +53,13 @@ def reconall(subjfile,*args):
     ext = os.path.splitext(filename)[-1].lower()
     if ext != ".nii":
         parser.error('File needs to be nifti format.')
-	
+
     wf = pe.Workflow(name="segment")
     wf.base_dir = T1dir
 	
     # run recon-all
     reconall = pe.Node(interface=fs.ReconAll(), name='reconall')
-    reconall.inputs.subject_id = filename[:4]
+    reconall.inputs.subject_id = subjID 
     reconall.inputs.directive = 'all'
     reconall.inputs.subjects_dir = subjdir
     reconall.inputs.T1_files = subjfile
