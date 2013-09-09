b40ab2685e3f36a45eb10b79f96698a330b9a63d
Grace <tsmgrace@gmail.com>
2013-09-09 12:15:15 -0700

    qc test

---
+++ MRS/tests/test_qc.py
@@ -0,0 +1,13 @@
+import os
+import MRS
+import MRS.qc as qc
+
+test_path = os.path.join(MRS.__path__[0], 'tests')
+ref_file = os.path.join(test_path, '5182_1_1.nii.gz')
+end_file = os.path.join(test_path, '5182_15_1.nii.gz')
+
+def test_motion():
+    """
+    test motion detection
+    """
+    G.qc(ref_file,end_file, thres=5.0)
