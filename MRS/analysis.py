fix glx2 fitting, start scaling to correct overestimation

MRS/analysis.py
@@ -502,7 +502,7 @@ def _do_two_lorentzian_fit(freqs, signal, bounds=None):
    return params
 
 
-def fit_two_gaussian(spectra, f_ppm, lb=3.6, ub=3.9):
+def fit_two_gaussian(spectra, f_ppm, lb=3.6, ub=3.9, scalefit=False):
    """
    Fit a gaussian function to the difference spectra to be used for estimation of
    the Glx peak.
@@ -517,6 +517,10 @@ def fit_two_gaussian(spectra, f_ppm, lb=3.6, ub=3.9):
    lb, ub: floats
       In ppm, the range over which optimization is bounded
 
+   scalefit : boolean
+      If this is set to true, attempt is made to prevent over or under-fitting
+      with a second round of fitting where the fitted curve is fit with
+      a scale factor. (default false)
    """
    idx = ut.make_idx(f_ppm, lb, ub)
    # We are only going to look at the interval between lb and ub
@@ -544,6 +548,9 @@ def fit_two_gaussian(spectra, f_ppm, lb=3.6, ub=3.9):
                                       bounds=bounds)
 
       model[ii] = fit_func(f_ppm[idx], *params[ii])
+
+   if scalefit:
+      scalefac, model = _do_scale_fit(f_ppm[idx], signal,model)
 
    return model, signal, params
 
@@ -619,7 +626,32 @@ def _do_two_gaussian_fit(freqs, signal, bounds=None):
                                 args=(freqs, np.real(signal),
                                 ut.two_gaussian, w, func_list),
                                 bounds=bounds)
+
    return params
+
+def _do_scale_fit(freqs, signal, model, w=None):
+   scalefac = np.empty(model.shape[0])
+   scalemodel = np.empty((model.shape[0], np.real(model).shape[1]))
+   scalesignal = np.empty((signal.shape[0], np.real(signal).shape[1]))
+   for ii, xx in enumerate(signal):
+      scalesignal[ii] = np.real(xx)
+      scalefac[ii], _ = lsq.leastsqbound(scaleerr, 1.0,
+                                      args=(freqs, np.real(scalesignal[ii]),
+                                      scalemodel[ii],w))
+      scalemodel[ii] = scalefac[ii] * model[ii]
+
+   return scalefac, scalemodel
+
+def scaleerr(scalefac, x, y, model, w=None):
+   err = y - model
+   if w is not None:
+      err = err * w
+   return err
+
+def scalemodel(model, scalefac):
+   for ii, mm in enumerate(model):
+      scaledmodel[ii] = mm * scalefac[ii]
+   return scaledmodel
 
 
 def fit_gaussian(spectra, f_ppm, lb=2.6, ub=3.6):

MRS/api.py
@@ -389,6 +389,10 @@ class GABA(object):
                  phase_correct=True):
         """
         Fit a Gaussian function to the NAA peak at ~ 2 ppm.
+        Example of fitting inverted peak:
+        Foerster et al. 2013, An imbalance between excitatory and
+        inhibitory neurotransmitters in amyothrophic lateral sclerosis
+        revealed by use of 3T proton MRS
         """
 
         model, signal, params = ana.fit_lorentzian(self.diff_spectra,
@@ -405,7 +409,7 @@ class GABA(object):
         self.naa_auc = self._calc_auc(ut.lorentzian, params, self.naa_idx)
 
 
-    def fit_glx2(self, reject_outliers=3.0, fit_lb=3.6, fit_ub=3.9, phase_correct=True):
+    def fit_glx2(self, reject_outliers=3.0, fit_lb=3.6, fit_ub=3.9, phase_correct=True, scalefit=False):
         """
         Fit a model to the portion of the diff spectra containing the
         glx signal. This treats the Glx signal as two gaussian peaks.
@@ -460,7 +464,7 @@ class GABA(object):
                                                                 ii)
 
         # We'll keep around a private attribute to tell us which transients
-        # were good (this is for both creatine and choline):
+        # were good:
         self._glx2_transients = np.where(ii)
 
         # Now we separate params of the two glx peaks from each other
@@ -482,13 +486,12 @@ class GABA(object):
             self.glxp2_model[idx] = ut.gaussian(self.f_ppm[self.glx2_idx],*self.glxp2_params[idx])
             self.glxp1_model[idx] = ut.gaussian(self.f_ppm[self.glx2_idx],
                                                     *self.glxp1_params[idx])
+
+        self.glx2_model = self.glxp1_model + self.glxp2_model
+
         self.glx2_signal = signal
-        self.glxp2_auc = self._calc_auc(ut.gaussian,
-                                           self.glxp2_params,
-                                           self.glx2_idx)
-        self.glxp1_auc = self._calc_auc(ut.gaussian,
-                                          self.glxp1_params,
-                                          self.glx2_idx)
+        self.glx2_auc = (self._calc_auc(ut.gaussian, self.glxp2_params, self.glx2_idx) +
+                        self._calc_auc(ut.gaussian, self.glxp1_params, self.glx2_idx))
 
     def est_gaba_conc(self):
         """